e9c5f58a4ad1910d273595f83d432a9b38b59ffa
Thomas Roehl <Thomas.Roehl@googlemail.com>
2021-06-08 15:54:15 +0200

    Add missing file for Fujitsu compilers

---
+++ make-toolchain-templates/include_FCC.mk
@@ -0,0 +1,15 @@
+CC  = fcc
+CXX = FCC
+FC  = frt
+LINKER = $(CC)
+
+ANSI_CFLAGS  = 
+
+CFLAGS   = -O2 -std=c99 -fPIC -g -Wall $(ANSI_CFLAGS)
+CXXFLAGS = $(CFLAGS)
+FCFLAGS  = -J ./$(TAG)
+VERSION  = -v
+LFLAGS   =
+DEFINES  = -D_GNU_SOURCE
+INCLUDES =
+LIBS     =
